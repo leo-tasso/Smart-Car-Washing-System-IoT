440ba4a58b2ff4380db299c4892af72cecf64357
Michele Olivieri <micheleolivieri02@gmail.com>
2023-11-16 19:20:17 +0100

    Start implementing ServoMotor utility class

---
+++ Arduino/Smart car washing system IoT/src/actuators/ServoMotorImpl.h
@@ -0,0 +1,20 @@
+#ifndef _SERVO_MOTOR_IMPL_
+#define _SERVO_MOTOR_IMPL_
+
+#include "ServoMotor.h"
+
+class ServoMotorImpl: public ServoMotor{
+
+private:
+    int pin;
+
+public:
+    ServoMotorImpl(int pin);
+
+    void on() = 0;
+    void off() = 0;
+    void setPosition(int angle) = 0;
+    
+};
+
+#endif
