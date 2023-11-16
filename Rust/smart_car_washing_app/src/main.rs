107dcd9e3a3b2cb792cf66078586daaad8fd431a
Leonardo Tassinari <tassinari.leonardo.2000@gmail.com>
2023-11-16 21:50:24 +0100

    add base rust program

---
+++ Rust/smart_car_washing_app/src/main.rs
@@ -0,0 +1,18 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use crate::app::SmartCarWashingApp;
+mod app;
+// When compiling natively:
+fn main() -> eframe::Result<()> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let native_options = eframe::NativeOptions {
+        initial_window_size: Some([400.0, 300.0].into()),
+        min_window_size: Some([300.0, 220.0].into()),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "eframe template",
+        native_options,
+        Box::new(|cc| Box::new(SmartCarWashingApp::new(cc))),
+    )
+}
