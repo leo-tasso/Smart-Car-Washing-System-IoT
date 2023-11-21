a9ecee853a9c117e46c98b4fa8f230dd2c199b73
Leonardo Tassinari <leonardo.tassinari6@studio.unibo.it>
2023-11-21 09:43:41 +0100

    add disabling maint in com task

--- Arduino/Smart car washing system IoT/src/task/communicator.cpp
+++ Arduino/Smart car washing system IoT/src/task/communicator.cpp
@@ -12,38 +12,41 @@ Communicator::Communicator(int period, CarWasher *carWasher)
 }
 
 void Communicator::tick() {
-    if(Serial.availableForWrite()){    
-    DynamicJsonDocument doc(128);
-    switch (carWasher->activeScenario) {
-        case SLEEP:
-            doc["Scenario"] = "Sleep";
-            break;
-        case WELCOME:
-            doc["Scenario"] = "Welcome";
-            break;
-        case ENTER:
-            doc["Scenario"] = "Enter";
-            break;
-        case FULL_ENTERED:
-            doc["Scenario"] = "FULL_ENTERED";
-            break;
-        case WASHING:
-            doc["Scenario"] = "Washing";
-            break;
-        case COMPLETE:
-            doc["Scenario"] = "Complete";
-            break;
-        case MAINT_REQ:
-            doc["Scenario"] = "Maintainece Required!";
-            break;
-        default:
-            break;
+    if (Serial.availableForWrite()) {
+        DynamicJsonDocument doc(128);
+        switch (carWasher->activeScenario) {
+            case SLEEP:
+                doc["Scenario"] = "Sleep";
+                break;
+            case WELCOME:
+                doc["Scenario"] = "Welcome";
+                break;
+            case ENTER:
+                doc["Scenario"] = "Enter";
+                break;
+            case FULL_ENTERED:
+                doc["Scenario"] = "FULL_ENTERED";
+                break;
+            case WASHING:
+                doc["Scenario"] = "Washing";
+                break;
+            case COMPLETE:
+                doc["Scenario"] = "Complete";
+                break;
+            case MAINT_REQ:
+                doc["Scenario"] = "Maintainece Required!";
+                break;
+            default:
+                break;
+        }
+        doc["Temp"] = carWasher->temp;
+        doc["WashedCars"] = carWasher->washedCars;
+        doc["RequiringMaint"] = carWasher->requiringManteinance;
+        doc["WashingPercentage"] = carWasher->washingPercentage;
+        serializeJson(doc, Serial);
+        Serial.flush();
     }
-    doc["Temp"] = carWasher->temp;
-    doc["WashedCars"] = carWasher->washedCars;
-    doc["RequiringMaint"] = carWasher->requiringManteinance;
-    doc["WashingPercentage"] = carWasher->washingPercentage;
-    serializeJson(doc, Serial);
-    Serial.flush();
+    if (Serial.available() > 0) {
+        carWasher->requiringManteinance = false;
     }
 };
