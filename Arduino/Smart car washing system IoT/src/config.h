e7e8bdebd021c910e11ad66431d4fe162560200a
Leonardo Tassinari <tassinari.leonardo.2000@gmail.com>
2023-11-25 18:16:05 +0100

    update config pin to match schematics

--- Arduino/Smart car washing system IoT/src/config.h
+++ Arduino/Smart car washing system IoT/src/config.h
@@ -1,16 +1,21 @@
 #ifndef __CONFIG__
 #define __CONFIG__
 
-constexpr int PIR_PIN = 10;
-constexpr int  L1_PIN = 11;
-constexpr int  L3_PIN = 11;
-constexpr int  START_BUTTON_PIN = 11;
+constexpr int TMP_PIN = A5;
+constexpr int PIR_PIN = 4;
+constexpr int L1_PIN = 7;
+constexpr int L2_PIN = 6;
+constexpr int L3_PIN = 5;
 constexpr int BASE_PERIOD = 50;
-constexpr int SERVO_PIN = 8;
+constexpr int SERVO_PIN = 3;
+constexpr int SONAR_TRIG_PIN = 11;
+constexpr int SONAR_ECHO_PIN = 12;
 
-constexpr int N1 = 1000; //ms
-constexpr int N2 = 500; //ms
-constexpr int N3 = 15000; //ms washing time
-constexpr int openingTime = 150; //ms
+constexpr int START_BUTTON_PIN = 2;
+
+constexpr int N1 = 1000;          // ms
+constexpr int N2 = 500;           // ms
+constexpr int N3 = 15000;         // ms washing time
+constexpr int openingTime = 150;  // ms
 
 #endif
