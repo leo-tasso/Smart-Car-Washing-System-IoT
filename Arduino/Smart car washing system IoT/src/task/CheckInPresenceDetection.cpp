92b4f5b25f9ee8cd410f4ffd1fd3fde44ce4cd8b
Leonardo Tassinari <leonardo.tassinari6@studio.unibo.it>
2023-11-29 10:25:56 +0100

    switched checkIn task with state

--- Arduino/Smart car washing system IoT/src/task/CheckInPresenceDetection.cpp
+++ Arduino/Smart car washing system IoT/src/task/CheckInPresenceDetection.cpp
@@ -1,22 +1,25 @@
 #include "task/checkInPresenceDetection.h"
 
 CheckInPresenceDetion::CheckInPresenceDetion(int period,
-                                                 CarWasher *carWasher,
-                                                 int pinPir,
-                                                 int pinL1)
-    : Task(period),
+                                             CarWasher *carWasher,
+                                             int pinPir,
+                                             int pinL1)
+    : TaskWithState(period),
       detector(new Pir(pinPir)),
       l1(new Led(pinL1)),
       carWasher(carWasher) {
 }
 
 void CheckInPresenceDetion::tick() {
-    this->state = detector->isDetected() ? DETECTED : UNDETECTED;
-    if (this->state == DETECTED) {
-        carWasher->carInCheckIn = true;
-        l1->turnOn();
-    } else {
-        l1->turnOff();
-        carWasher->carInCheckIn = false;
+    this->setState(detector->isDetected() ? CheckInStates::DETECTED : CheckInStates::UNDETECTED);
+    switch (this->getState()) {
+        case CheckInStates::DETECTED:
+            carWasher->carInCheckIn = true;
+            l1->turnOn();
+            break;
+        case CheckInStates::UNDETECTED:
+            l1->turnOff();
+            carWasher->carInCheckIn = false;
+            break;
     }
 };
--- Arduino/Smart car washing system IoT/src/task/checkInPresenceDetection.h
+++ Arduino/Smart car washing system IoT/src/task/checkInPresenceDetection.h
@@ -4,17 +4,17 @@
 #include "CarWasher.h"
 #include "actuators/Led.h"
 #include "sensors/Pir.h"
-#include "system/task.h"
+#include "system/taskWithState.h"
 
-class CheckInPresenceDetion : public Task {
+    enum class CheckInStates {
+        DETECTED,
+        UNDETECTED
+    };
+class CheckInPresenceDetion : public TaskWithState<CheckInStates> {
    private:
     PresenceDetector *detector;
     Light *l1;
     CarWasher *carWasher;
-    enum {
-        DETECTED,
-        UNDETECTED
-    } state{UNDETECTED};
 
    public:
     CheckInPresenceDetion(int period, CarWasher *CarWasher, int pinPir, int pinL1);
