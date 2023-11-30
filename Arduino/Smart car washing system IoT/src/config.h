ede277513a342c3a05768a53145fdc947918aa32
Leonardo Tassinari <tassinari.leonardo.2000@gmail.com>
2023-11-30 21:01:53 +0100

    adjust distance value

--- Arduino/Smart car washing system IoT/src/config.h
+++ Arduino/Smart car washing system IoT/src/config.h
@@ -23,7 +23,7 @@ constexpr int N5 = 5000;             // ms exit time
 constexpr int SLEEP_TIME = 10000;    // ms
 constexpr int transitionTime = 500;  // ms opening/closing time of the gate
 constexpr double minDist = 0.07;     // 100cm = 1m
-constexpr double maxDist = 0.09;     // 300cm = 3m
+constexpr double maxDist = 0.20;     // 300cm = 3m
 constexpr int noObjDetected = -1;
 constexpr int MAXTEMP = 40;
 constexpr unsigned int PERIOD_L2_GATE = 300;
